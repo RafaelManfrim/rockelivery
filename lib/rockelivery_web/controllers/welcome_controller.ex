10396e6548f1ebb7f44ba3a16d45778eab3ac605
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-23 23:21:50 -0300

    Testando rotas

---
+++ lib/rockelivery_web/controllers/welcome_controller.ex
@@ -0,0 +1,7 @@
+defmodule RockeliveryWeb.WelcomeController do
+  use RockeliveryWeb, :controller
+
+  def index(conn, params) do
+    conn |> put_status(:ok) |> text("Welcome to Rockelivery Web!")
+  end
+end
