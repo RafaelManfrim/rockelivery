876f6431e4939760b84a82eafa4fb43a24604113
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-24 23:42:17 -0300

    implementando rota de update

--- lib/rockelivery_web/controllers/users_controller.ex
+++ lib/rockelivery_web/controllers/users_controller.ex
@@ -22,4 +22,10 @@ defmodule RockeliveryWeb.UsersController do
       conn |> put_status(:ok) |> render("show.json", user: user)
     end
   end
+
+  def update(conn, params) do
+    with {:ok, %User{} = user} <- Rockelivery.update_user(params) do
+      conn |> put_status(:ok) |> render("show.json", user: user)
+    end
+  end
 end
