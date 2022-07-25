e81d5fb45fd5a2222ffe5a86adcd55a106a64ebe
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-24 22:37:21 -0300

    criando rota de leitura de usuário

--- lib/rockelivery_web/controllers/users_controller.ex
+++ lib/rockelivery_web/controllers/users_controller.ex
@@ -10,4 +10,10 @@ defmodule RockeliveryWeb.UsersController do
       conn |> put_status(:created) |> render("create.json", user: user)
     end
   end
+
+  def show(conn, %{"id" => id}) do
+    with {:ok, %User{} = user} <- Rockelivery.get_user_by_id(id) do
+      conn |> put_status(:ok) |> render("show.json", user: user)
+    end
+  end
 end
