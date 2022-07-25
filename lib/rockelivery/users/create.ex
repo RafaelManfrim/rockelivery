5efbb51a0e29f0c9505411a3db19cf8974cfa4c2
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-24 22:16:58 -0300

    flexibilizando exibição de error

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -2,6 +2,9 @@ defmodule Rockelivery.Users.Create do
   alias Rockelivery.{Repo, User}
 
   def call(params) do
-    params |> User.changeset() |> Repo.insert()
+    params |> User.changeset() |> Repo.insert() |> handle_insert()
   end
+
+  defp handle_insert({:ok, %User{}} = result), do: result
+  defp handle_insert({:error, result}), do: {:error, %{status: :bad_request, result: result}}
 end
--- lib/rockelivery_web/controllers/fallback_controller.ex
+++ lib/rockelivery_web/controllers/fallback_controller.ex
@@ -2,7 +2,7 @@ defmodule RockeliveryWeb.FallbackController do
   use RockeliveryWeb, :controller
   alias RockeliveryWeb.ErrorView
 
-  def call(conn, {:error, result}) do
-    conn |> put_status(:bad_request) |> put_view(ErrorView) |> render("400.json", result: result)
+  def call(conn, {:error, %{status: status, result: result}}) do
+    conn |> put_status(status) |> put_view(ErrorView) |> render("error.json", result: result)
   end
 end
