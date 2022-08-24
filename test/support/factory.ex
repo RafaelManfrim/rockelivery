97bd10be622efda888c83fa77e9fae5b04dbd5e6
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-08-23 23:40:18 -0300

    Ajustando testes

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -4,13 +4,13 @@ defmodule Rockelivery.Factory do
 
   def user_params_factory do
     %{
-      age: 27,
-      address: "Rua Carvalho",
-      cep: "12345678",
-      cpf: "11111111111",
-      name: "Rafael",
-      password: "123456",
-      email: "rafael@rockelivery.com"
+      "age" => 27,
+      "address" => "Rua Carvalho",
+      "cep" => "12345678",
+      "cpf" => "11111111111",
+      "name" => "Rafael",
+      "password" => "123456",
+      "email" => "rafael@rockelivery.com"
     }
   end
 
